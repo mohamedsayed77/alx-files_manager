114d661a497123bdd387a034d5264e34a293fa75
mohamedsayed77 <mohamed.gemy0007@gmail.com>
2024-12-09 03:59:44 +0200

    alx-files_manager

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,22 +1,27 @@
-import redisClient from '../utils/redis';
 import dbClient from '../utils/db';
+import redisClient from '../utils/redis';
 
-class AppController {
-  // Gets the status of redis and db
-  static async getStatus(req, res) {
-    const redisIsAlive = redisClient.isAlive();
-    const dbIsAlive = dbClient.isAlive();
-    const status = { redis: redisIsAlive, db: dbIsAlive };
-    await res.status(200).json(status);
-  }
+const AppController = {
+  async getStatus(req, res) {
+    const redisStatus = redisClient.isAlive();
+    const dbStatus = dbClient.isAlive();
 
-  // Gets the stats of of the users and files in DB
-  static async getStats(req, res) {
-    const usersCount = await dbClient.nbUsers();
-    const filesCount = await dbClient.nbFiles();
-    const stats = { users: usersCount, files: filesCount };
-    res.status(200).json(stats);
-  }
-}
+    if (redisStatus && dbStatus) {
+      res.status(200).json({ redis: true, db: true });
+    } else {
+      res.status(500).json({ redis: redisStatus, db: dbStatus });
+    }
+  },
+
+  async getStats(req, res) {
+    try {
+      const usersCount = await dbClient.nbUsers();
+      const filesCount = await dbClient.nbFiles();
+      res.status(200).json({ users: usersCount, files: filesCount });
+    } catch (error) {
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+  },
+};
 
 export default AppController;
--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,42 +1,65 @@
 import sha1 from 'sha1';
-import dbClient from '../utils/db';
 import redisClient from '../utils/redis';
-import { ObjectId } from 'mongodb';
-import Queue from 'bull';
+import dbClient from '../utils/db';
+
+const { userQueue } = require('../worker');
 
 class UsersController {
-  // eslint-disable-next-line consistent-return
   static async postNew(req, res) {
-    const queue = new Queue('userQueue');
-    const { email, password } = req.body;
-    if (!email) return res.status(400).json({ error: 'Missing email' });
-    if (!password) return res.status(400).json({ error: 'Missing password' });
+    try {
+      const { email, password } = req.body;
 
-    const users = await dbClient.db.collection('users');
-    users.findOne({ email }, async (err, result) => {
-      if (result) {
-        return res.status(400).json({ error: 'Already exist' });
+      if (!email) {
+        return res.status(400).json({ error: 'Missing email' });
       }
+
+      if (!password) {
+        return res.status(400).json({ error: 'Missing password' });
+      }
+
+      const userExists = await dbClient.db.collection('users').findOne({ email });
+
+      if (userExists) {
+        return res.status(400).json({ error: 'Already exists' });
+      }
+
       const hashedPassword = sha1(password);
-      const { insertedId } = await users.insertOne({ email, password: hashedPassword });
-      const user = { id: insertedId, email };
-      queue.add({ userId: insertedId });
-      return res.status(201).json(user);
-    });
+      const newUser = {
+        email,
+        password: hashedPassword,
+      };
+
+      const result = await dbClient.db.collection('users').insertOne(newUser);
+      const { _id } = result.ops[0];
+
+      await userQueue.add({ userId: _id });
+
+      return res.status(201).json({ id: _id, email });
+    } catch (error) {
+      console.error(error);
+      return res.status(500).json({ error: 'Internal Server Error' });
+    }
   }
 
   static async getMe(req, res) {
-    const token = req.header('X-Token');
-    const id = await redisClient.get(`auth_${token}`);
-    if (id) {
-      const user = await dbClient.db.collection('users').findOne({ _id: ObjectId(id) });
-      if (user) {
-        return res.status(200).json({ id: user._id, email: user.email });
-      } else {
+    try {
+      const { token } = req.headers;
+      const userId = await redisClient.get(`auth_${token}`);
+
+      if (!userId) {
         return res.status(401).json({ error: 'Unauthorized' });
       }
-    } else {
-      return res.status(401).json({ error: 'Unauthorized' });
+
+      const user = await dbClient.db.collection('users').findOne({ _id: userId });
+
+      if (!user) {
+        return res.status(401).json({ error: 'Unauthorized' });
+      }
+
+      return res.status(200).json({ id: user._id, email: user.email });
+    } catch (error) {
+      console.error(error);
+      return res.status(500).json({ error: 'Internal Server Error' });
     }
   }
 }
